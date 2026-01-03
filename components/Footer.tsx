cfa4dee8061e827b0267f83348023fa2e2891a48
PC <mar.sosaer5@gmail.com>
2026-01-02 23:33:51 -0500

    Agregamos componentes necesarios para la web

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
-import { logoutAccount } from '@/lib/actions/user.actions';
+import { logoutAccount } from '@/lib/actions/user.actions'
 import Image from 'next/image'
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const Footer = ({ user, type = 'desktop' }: FooterProps) => {
@@ -9,29 +9,28 @@ const Footer = ({ user, type = 'desktop' }: FooterProps) => {
     const handleLogOut = async () => {
         const loggedOut = await logoutAccount();
 
-        if(loggedOut) router.push('/sign-in')
+        if (loggedOut) router.push('/sign-in')
     }
 
     return (
         <footer className="footer">
-            <div className={type === 'mobile' ?  'footer_name-mobile' : 'footer_name'}>
+            <div className={type === 'mobile' ? 'footer_name-mobile' : 'footer_name'}>
                 <p className="text-xl font-bold text-gray-700">
-                    {user?.name[0]}
+                    {user?.firstName[0]}
                 </p>
             </div>
 
-            <div className={type === 'mobile' ?  'footer_email-mobile' : 'footer_email'}>
-                <h1 className="text-14 truncate font-normal text-gray-700 font-semibold">
-                    {user?.name}
+            <div className={type === 'mobile' ? 'footer_email-mobile' : 'footer_email'}>
+                <h1 className="text-14 truncate text-gray-700 font-semibold">
+                    {user?.firstName}
                 </h1>
-
                 <p className="text-14 truncate font-normal text-gray-600">
                     {user?.email}
                 </p>
             </div>
 
             <div className="footer_image" onClick={handleLogOut}>
-                <Image src="icons/logout.svg" fill alt="sahe"/>
+                <Image src="icons/logout.svg" fill alt="jsm" />
             </div>
         </footer>
     )
--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -3,6 +3,7 @@ import { Button } from './ui/button'
 import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from 'react-plaid-link'
 import { useRouter } from 'next/navigation';
 import { createLinkToken, exchangePublicToken } from '@/lib/actions/user.actions';
+import Image from 'next/image';
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
     const router = useRouter();
@@ -21,39 +22,53 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
 
     const onSuccess = useCallback<PlaidLinkOnSuccess>(async (public_token: string) => {
         await exchangePublicToken({
-            publicToken: public_token, 
-            user})
+            publicToken: public_token,
+            user,
+        })
+
         router.push('/');
-        }, [user])
-    
+    }, [user])
+
     const config: PlaidLinkOptions = {
         token,
         onSuccess
     }
 
     const { open, ready } = usePlaidLink(config);
+
     return (
         <>
             {variant === 'primary' ? (
                 <Button
-                onClick={() => open()}
-                disabled={!ready}
+                    onClick={() => open()}
+                    disabled={!ready}
                     className="plaidlink-primary"
                 >
-                    Conectar Banco
+                    Connect bank
                 </Button>
-            ): variant === 'ghost' ? (
-                <Button>
-                    Conectar Banco
+            ) : variant === 'ghost' ? (
+                <Button onClick={() => open()} variant="ghost" className="plaidlink-ghost">
+                    <Image
+                        src="/icons/connect-bank.svg"
+                        alt="connect bank"
+                        width={24}
+                        height={24}
+                    />
+                    <p className='hiddenl text-[16px] font-semibold text-black-2 xl:block'>Connect bank</p>
                 </Button>
-            ): (
-                <Button>
-                    Conectar Banco
+            ) : (
+                <Button onClick={() => open()} className="plaidlink-default">
+                    <Image
+                        src="/icons/connect-bank.svg"
+                        alt="connect bank"
+                        width={24}
+                        height={24}
+                    />
+                    <p className='text-[16px] font-semibold text-black-2'>Connect bank</p>
                 </Button>
-            )} 
+            )}
         </>
     )
 }
 
 export default PlaidLink
-
